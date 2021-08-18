28e4f16cecc20f6c1560e0d1a2fd38c37b7bba5d
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-08-18 16:22:34 +0430

    Create test.py

---
+++ Book/os/test.py
@@ -0,0 +1,33 @@
+"""
+os Python Library
+"""
+import os # import os
+
+# Module And Objects
+# print(os.name) # Show Your OS Name
+# nt = Windows
+# posix = Linux
+# print(os.getcwd()) # Show Your Path
+# -----
+# os.mkdir("folder") # Make Folder
+# os.rmdir("folder") # Remove Dir
+# ------
+#            File Name
+# os.remove("text.txt") # remove file
+# -------
+"""
+OS System Module
+"""
+#          Command
+os.system("cls")
+# -----
+#       File Name
+os.chmod("text.txt",777) # Change Mode
+
+
+
+"""
+Python OS Library
+
+Mr.Nope: ;)
+"""
